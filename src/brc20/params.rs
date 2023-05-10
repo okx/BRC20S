db6dad8e3073721dd7a89f113cfa46119707b9ba
fatcat22 <fatcat22@noreplay.github.com>
2023-05-10 16:58:39 +0800

    fix decmial

--- src/brc20/params.rs
+++ src/brc20/params.rs
@@ -5,8 +5,9 @@ pub const PROTOCOL_LITERAL: &str = "brc-20";
 pub const MAX_DECIMAL_WIDTH: u8 = 18;
 pub const TICK_BYTE_COUNT: usize = 4;
 
-pub static MAXIMUM_SUPPLY: Lazy<Num> =
-  Lazy::new(|| Num::from_str_radix("FFFFFFFFFFFFFFFF", 16).unwrap());
+pub static MAXIMUM_SUPPLY: Lazy<Num> = Lazy::new(|| Num::from(u64::MAX));
+
+pub static BIGDECIMAL_TEN: Lazy<Num> = Lazy::new(|| Num::from(10u64));
 
 pub const fn default_decimals() -> u8 {
   MAX_DECIMAL_WIDTH

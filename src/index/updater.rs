11ff3d677f0407834b277c0fa0efda9c23ad9fe7
BananaLF <864685021@qq.com>
2023-06-13 17:58:05 +0800

    Sunstrider/fix compile (#6)
    * remove value_cache
    
    * solve a bug

--- src/index/updater.rs
+++ src/index/updater.rs
@@ -150,8 +150,7 @@ impl Updater {
         // commit must be done before making savepoint
         // do not make savepoint in fast sync mode
         if !is_fast_sync && self.height % SAVEPOINT_INTERVAL == 0 {
-          self.commit(wtx, value_cache)?;
-          value_cache = HashMap::new();
+          self.commit(wtx)?;
           uncommitted = 0;
           wtx = index.begin_write()?;
           let sp = wtx.savepoint()?;

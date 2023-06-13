31797d4d4d5147d83de50eaf412a92f2ba8868d0
BananaLF <864685021@qq.com>
2023-06-13 17:55:42 +0800

    solve a bug

--- src/index/updater.rs
+++ src/index/updater.rs
@@ -150,7 +150,7 @@ impl Updater {
         // commit must be done before making savepoint
         // do not make savepoint in fast sync mode
         if !is_fast_sync && self.height % SAVEPOINT_INTERVAL == 0 {
-          self.commit(wt)?;
+          self.commit(wtx)?;
           uncommitted = 0;
           wtx = index.begin_write()?;
           let sp = wtx.savepoint()?;

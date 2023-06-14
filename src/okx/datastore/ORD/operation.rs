220b83f748dcb5fd8d6e4d428328430c3dab1873
jianguo <zjg555543@163.com>
2023-06-14 12:16:41 +0800

    update

---
+++ src/okx/datastore/ORD/operation.rs
@@ -0,0 +1,17 @@
+use crate::{InscriptionId, SatPoint};
+use bitcoin::Txid;
+
+// collect the inscription operation.
+pub struct Operation {
+  pub txid: Txid,
+  pub action: Action,
+  pub inscription_id: InscriptionId,
+  pub old_satpoint: SatPoint,
+  pub new_satpoint: SatPoint,
+}
+
+// the act of marking an inscription.
+pub enum Action {
+  New,
+  Transfer,
+}

44e530e8ec37eb91eeefaa31ba85f1db564a79f9
jianguo <zjg555543@163.com>
2023-06-15 17:19:06 +0800

    update

--- src/okx/reward/error.rs
+++ src/okx/reward/error.rs
@@ -4,4 +4,10 @@ use serde::{Deserialize, Serialize};
 pub enum RewardError {
   #[error("invalid number: {0}")]
   InvalidNum(String),
+
+  #[error("no stake: pid:{0}, user:{0}")]
+  NoStaked(String, String),
+
+  #[error("calculate overflow")]
+  Overflow(),
 }
--- src/okx/reward/reward.rs
+++ src/okx/reward/reward.rs
@@ -1,12 +1,29 @@
 use super::error::RewardError;
 use crate::okx::datastore::ScriptKey;
-use crate::okx::datastore::BRC30::pool_info::Pid;
-use std::collections::HashMap;
-use std::time;
+use crate::okx::datastore::BRC30::{
+  BRC30DataStoreReadOnly, BRC30DataStoreReadWrite, Balance, InscriptionOperation, Pid, PledgedTick,
+  PoolInfo, Receipt, StakeInfo, TickId, TickInfo, TransferableAsset, UserInfo,
+};
 
-struct Pool {}
+use crate::okx::datastore::{
+  BRC20::redb::BRC20DataStore, BRC30::redb::BRC30DataStore, ORD::OrdDbReader,
+};
 
-impl Pool {
+use redb::WriteTransaction;
+
+pub struct Pool<'db, 'a> {
+  wtx: &'a WriteTransaction<'db>,
+  brc30db: BRC30DataStore<'db, 'a>,
+}
+
+impl<'db, 'a> Pool<'db, 'a> {
+  pub fn new(wtx: &'a WriteTransaction<'db>) -> Self {
+    let brc30db = BRC30DataStore::new(&wtx);
+    Self { wtx, brc30db }
+  }
+}
+
+impl<'db, 'a> Pool<'db, 'a> {
   pub fn query_reward(&mut self, pid: &Pid, script_key: &ScriptKey) -> (String, u128) {
     //TODO query_reward
     return (script_key.to_string(), 1u128);
@@ -48,10 +65,48 @@ impl Pool {
     pid: &Pid,
     script_key: &ScriptKey,
   ) -> Result<u128, RewardError> {
-    // return Err(RewardError::InvalidNum("er".to_string()));
     //1. 判断用户的 staked 是否大于0
+    let mut user = self
+      .brc30db
+      .get_pid_to_use_info(&script_key.clone(), &pid.clone())
+      .unwrap()
+      .unwrap();
+
+    if user.staked <= 0 {
+      return Err(RewardError::NoStaked(
+        pid.to_lowercase().hex(),
+        script_key.to_string(),
+      ));
+    }
+
+    let mut pool = self
+      .brc30db
+      .get_pid_to_poolinfo(&pid.clone())
+      .unwrap()
+      .unwrap();
 
     //2. 根据用户的质押数计算收益： staked * accRewardPerShare - user reward_debt，并存储
+    let mut a = 0;
+    match user.staked.checked_mul(pool.acc_reward_per_share) {
+      Some(result) => a = result,
+      None => println!("Multiplication failed!"),
+    }
+
+    let mut reward: u128 = 0;
+    match a.checked_sub(user.reward_debt) {
+      Some(result) => reward = result,
+      None => println!("Division failed!"),
+    }
+
+    if reward > 0 {
+      //3 更新 user_info 和pool 的 minted，TODO check overflow
+      user.reward += reward;
+      pool.minted += reward;
+      self
+        .brc30db
+        .set_pid_to_use_info(&script_key.clone(), &pid.clone(), &user);
+      self.brc30db.set_pid_to_poolinfo(&pid.clone(), &pool);
+    }
 
     return Ok(0u128);
   }
@@ -78,10 +133,16 @@ impl Pool {
 mod tests {
   use super::*;
   use bitcoin::Address;
+  use redb::Database;
   use std::str::FromStr;
+  use tempfile::NamedTempFile;
   #[test]
   fn test_flow() {
-    let mut pool = Pool {};
+    let dbfile = NamedTempFile::new().unwrap();
+    let db = Database::create(dbfile.path()).unwrap();
+    let wtx = db.begin_write().unwrap();
+
+    let mut pool = Pool::new(&wtx);
 
     let pid = Pid::from_str("Bca1DaBca1D#1").unwrap();
     let script_key =
@@ -90,12 +151,12 @@ mod tests {
     // 1.from deposit, withdraw
 
     // 2.update_pool
-    pool.update_pool(&pid, 0u64);
+    // pool.update_pool(&pid, 0u64);
 
     // 2.if no stake, return 0
-    pool.withdraw_user_reward(&pid, &script_key);
+    // pool.withdraw_user_reward(&pid, &script_key);
 
     // 3. calculate and update the user's new stake.
-    pool.update_user_stake(&pid, &script_key, 0u128, true);
+    // pool.update_user_stake(&pid, &script_key, 0u128, true);
   }
 }

0087a5ea936fc09b300e86744d8b68820105d33d
jianguo <zjg555543@163.com>
2023-06-19 17:48:19 +0800

    optimize code

--- src/okx/datastore/balance.rs
+++ src/okx/datastore/balance.rs
@@ -1,45 +1,34 @@
-use crate::okx::datastore::ScriptKey;
-use crate::okx::datastore::BRC20::balance::Balance as BRC20Balance;
-use crate::okx::datastore::BRC20::redb::BRC20DataStore;
-use crate::okx::datastore::BRC20::BRC20Error::InsufficientBalance;
-use crate::okx::datastore::BRC20::{BRC20DataStoreReadOnly, BRC20DataStoreReadWrite, BRC20Error};
-use crate::okx::datastore::BRC30::{
-  BRC30DataStoreReadOnly, Balance as BRC30Balance, Pid, UserInfo,
-};
-use crate::okx::datastore::BRC30::{BRC30DataStoreReadWrite, PledgedTick};
-use crate::okx::protocol::BRC30::params::{BIGDECIMAL_TEN, NATIVE_TOKEN_DECIMAL};
-use crate::okx::protocol::BRC30::{BRC30Error, Error, Num};
+use std::str::FromStr;
 use anyhow::anyhow;
 use bigdecimal::num_bigint::Sign;
-use std::str::FromStr;
+use crate::okx::datastore::BRC20::{BRC20DataStoreReadOnly, BRC20DataStoreReadWrite, BRC20Error};
+use crate::okx::datastore::BRC20::BRC20Error::InsufficientBalance;
+use crate::okx::datastore::BRC20::redb::BRC20DataStore;
+use crate::okx::datastore::BRC20::balance::Balance as BRC20Balance;
+use crate::okx::datastore::BRC30::{Balance as BRC30Balance, BRC30DataStoreReadOnly, Pid, UserInfo};
+use crate::okx::datastore::BRC30::{ BRC30DataStoreReadWrite, PledgedTick};
+use crate::okx::datastore::ScriptKey;
+use crate::okx::protocol::BRC30::{BRC30Error, Error, Num};
+use crate::okx::protocol::BRC30::params::{BIGDECIMAL_TEN, NATIVE_TOKEN_DECIMAL};
 
-pub fn get_user_common_balance<'a, L: BRC30DataStoreReadWrite, M: BRC20DataStoreReadWrite>(
-  script: &ScriptKey,
-  token: &PledgedTick,
-  brc30ledger: &'a L,
-  brc20ledger: &'a M,
-) -> Num {
+
+pub fn get_user_common_balance<'a, L: BRC30DataStoreReadWrite, M:BRC20DataStoreReadWrite>
+(script: &ScriptKey,token: &PledgedTick, brc30ledger: &'a L, brc20ledger: &'a M) -> Num {
   match token {
-    PledgedTick::NATIVE => Num::from(0_u128),
+    PledgedTick::NATIVE => {Num::from(0_u128)},
     PledgedTick::BRC30Tick(tickid) => {
       let balance = brc30ledger
-        .get_balance(&script, &tickid)
-        .map_or(Some(BRC30Balance::default(tickid)), |v| v)
-        .unwrap();
-
-      let dec = brc30ledger.get_tick_info(&tickid).unwrap().unwrap().decimal;
+        .get_balance(&script,&tickid)
+        .map_or(Some(BRC30Balance::default(tickid)), |v | v).unwrap();
       Num::from(balance.overall_balance)
-    }
+    },
     PledgedTick::BRC20Tick(tick) => {
       let balance = brc20ledger
-        .get_balance(&script, tick)
-        .map_or(Some(BRC20Balance::new()), |v| v)
-        .unwrap();
-
-      let dec = brc20ledger.get_token_info(tick).unwrap().unwrap().decimal;
+        .get_balance(&script,tick)
+        .map_or(Some(BRC20Balance::new()),|v|v).unwrap();
       Num::from(balance.overall_balance)
-    }
-    PledgedTick::UNKNOWN => Num::from(0_u128),
+    },
+    PledgedTick::UNKNOWN => {Num::from(0_u128)}
   }
 }
 
@@ -55,35 +44,32 @@ pub fn get_stake_dec<'a, L: BRC30DataStoreReadWrite, M: BRC20DataStoreReadWrite>
     PledgedTick::UNKNOWN => 0_u8,
   }
 }
-
-pub fn stake_is_exist<'a, L: BRC30DataStoreReadWrite, M: BRC20DataStoreReadWrite>(
-  token: &PledgedTick,
-  brc30ledger: &'a L,
-  brc20ledger: &'a M,
-) -> bool {
+pub fn stake_is_exist<'a, L: BRC30DataStoreReadWrite, M:BRC20DataStoreReadWrite>
+(token: &PledgedTick, brc30ledger: &'a L, brc20ledger: &'a M)
+ -> bool {
   match token {
-    PledgedTick::NATIVE => true,
+    PledgedTick::NATIVE => {true},
     PledgedTick::BRC30Tick(tickid) => {
       let tickinfo = brc30ledger.get_tick_info(&tickid);
       match tickinfo {
         Ok(info) => match info {
-          Some(_) => true,
-          _ => false,
-        },
-        _ => false,
+          Some(_) => {true}
+          _ => {false}
+        }
+        _ => {false}
       }
-    }
+    },
     PledgedTick::BRC20Tick(tick) => {
       let tokeninfo = brc20ledger.get_token_info(&tick);
       match tokeninfo {
         Ok(info) => match info {
-          Some(_) => true,
-          _ => false,
-        },
-        _ => false,
+          Some(_) => {true}
+          _ => {false}
+        }
+        _ => {false}
       }
-    }
-    PledgedTick::UNKNOWN => false,
+    },
+    PledgedTick::UNKNOWN => {false}
   }
 }
 
@@ -101,42 +87,31 @@ pub fn stake_is_exist<'a, L: BRC30DataStoreReadWrite, M: BRC20DataStoreReadWrite
 //   Ok(balance-userinfo.staked)
 // }
 
-pub fn convert_pledged_tick_with_decimal<
-  'a,
-  L: BRC30DataStoreReadWrite,
-  M: BRC20DataStoreReadWrite,
->(
-  tick: &PledgedTick,
-  amount: &str,
-  brc30ledger: &'a L,
-  brc20ledger: &'a M,
-) -> Result<Num, Error<L>> {
+pub fn convert_pledged_tick_with_decimal<'a, L: BRC30DataStoreReadWrite, M: BRC20DataStoreReadWrite>
+(tick: &PledgedTick, amount: &str, brc30ledger: &'a L, brc20ledger: &'a M)
+ -> Result<Num, Error<L>> {
   match tick {
-    PledgedTick::UNKNOWN => Err(Error::BRC30Error(BRC30Error::UnknownStakeType)),
-    PledgedTick::NATIVE => convert_amount_with_decimal(amount, NATIVE_TOKEN_DECIMAL),
+    PledgedTick::UNKNOWN => { Err(Error::BRC30Error(BRC30Error::UnknownStakeType)) },
+    PledgedTick::NATIVE => { convert_amount_with_decimal(amount, NATIVE_TOKEN_DECIMAL) },
     PledgedTick::BRC20Tick(tick) => {
-      let token = brc20ledger
-        .get_token_info(tick)
-        .map_err(|e| Error::Others(anyhow!("brc20_query failed:{}", e)))?
+      let token = brc20ledger.get_token_info(tick)
+        .map_err(|e|
+          Error::Others(anyhow!("brc20_query failed:{}",e)))?
         .ok_or(BRC30Error::TickNotFound(tick.hex()))?;
 
       convert_amount_with_decimal(amount, token.decimal)
-    }
+    },
     PledgedTick::BRC30Tick(tickid) => {
-      let tick = brc30ledger
-        .get_tick_info(tickid)
+      let tick = brc30ledger.get_tick_info(tickid)
         .map_err(|e| Error::LedgerError(e))?
         .ok_or(BRC30Error::TickNotFound(tickid.to_lowercase().hex()))?;
 
       convert_amount_with_decimal(amount, tick.decimal)
-    }
+    },
   }
 }
 
-pub fn convert_amount_with_decimal<L: BRC30DataStoreReadWrite>(
-  amount: &str,
-  decimal: u8,
-) -> Result<Num, Error<L>> {
+pub fn convert_amount_with_decimal<L: BRC30DataStoreReadWrite>(amount:&str,decimal:u8) -> Result<Num,Error<L>> {
   let base = BIGDECIMAL_TEN.checked_powu(decimal as u64)?;
   let mut amt = Num::from_str(amount)?;
 
@@ -152,42 +127,32 @@ pub fn convert_amount_with_decimal<L: BRC30DataStoreReadWrite>(
   Ok(amt)
 }
 
-pub fn convert_pledged_tick_without_decimal<
-  'a,
-  L: BRC30DataStoreReadWrite,
-  M: BRC20DataStoreReadWrite,
->(
-  tick: &PledgedTick,
-  amount: u128,
-  brc30ledger: &'a L,
-  brc20ledger: &'a M,
-) -> Result<Num, Error<L>> {
+
+pub fn convert_pledged_tick_without_decimal<'a, L: BRC30DataStoreReadWrite, M: BRC20DataStoreReadWrite>
+(tick: &PledgedTick, amount: u128, brc30ledger: &'a L, brc20ledger: &'a M)
+ -> Result<Num, Error<L>> {
   match tick {
-    PledgedTick::UNKNOWN => Err(Error::BRC30Error(BRC30Error::UnknownStakeType)),
-    PledgedTick::NATIVE => convert_amount_without_decimal(amount, NATIVE_TOKEN_DECIMAL),
+    PledgedTick::UNKNOWN => { Err(Error::BRC30Error(BRC30Error::UnknownStakeType)) },
+    PledgedTick::NATIVE => { convert_amount_without_decimal(amount, NATIVE_TOKEN_DECIMAL) },
     PledgedTick::BRC20Tick(tick) => {
-      let token = brc20ledger
-        .get_token_info(tick)
-        .map_err(|e| Error::Others(anyhow!("brc20_query failed:{}", e)))?
+      let token = brc20ledger.get_token_info(tick)
+        .map_err(|e|
+          Error::Others(anyhow!("brc20_query failed:{}",e)))?
         .ok_or(BRC30Error::TickNotFound(tick.hex()))?;
 
       convert_amount_without_decimal(amount, token.decimal)
-    }
+    },
     PledgedTick::BRC30Tick(tickid) => {
-      let tick = brc30ledger
-        .get_tick_info(tickid)
+      let tick = brc30ledger.get_tick_info(tickid)
         .map_err(|e| Error::LedgerError(e))?
         .ok_or(BRC30Error::TickNotFound(tickid.to_lowercase().hex()))?;
 
       convert_amount_without_decimal(amount, tick.decimal)
-    }
+    },
   }
 }
 
-pub fn convert_amount_without_decimal<L: BRC30DataStoreReadWrite>(
-  amount: u128,
-  decimal: u8,
-) -> Result<Num, Error<L>> {
+pub fn convert_amount_without_decimal<L: BRC30DataStoreReadWrite>(amount:u128,decimal:u8) -> Result<Num,Error<L>> {
   let base = BIGDECIMAL_TEN.checked_powu(decimal as u64)?;
   let mut amt = Num::from(amount);
 

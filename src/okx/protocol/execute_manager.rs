269ed93f2e369b643e548cc1f0b4115aef03e329
BananaLF <864685021@qq.com>
2023-06-19 16:46:40 +0800

    add brc30store. deal with brc20 transfer for brc30 passive

--- src/okx/protocol/execute_manager.rs
+++ src/okx/protocol/execute_manager.rs
@@ -1,20 +1,34 @@
 use super::*;
-use crate::okx::datastore::BRC20::BRC20Receipt;
+use crate::okx::datastore::balance::{
+  convert_amount_with_decimal, convert_pledged_tick_with_decimal,
+};
+use crate::okx::datastore::BRC30DataStoreReadWrite;
+use crate::okx::datastore::BRC20::{BRC20Event, BRC20Receipt};
+use crate::okx::protocol::BRC30::{Operation as BRC30Operation, PassiveUnStake};
 use crate::{
   okx::datastore::{BRC20DataStoreReadWrite, OrdDataStoreReadWrite},
   Result,
 };
 
-pub struct CallManager<'a, O: OrdDataStoreReadWrite, N: BRC20DataStoreReadWrite> {
+pub struct CallManager<
+  'a,
+  O: OrdDataStoreReadWrite,
+  N: BRC20DataStoreReadWrite,
+  M: BRC30DataStoreReadWrite,
+> {
   ord_store: &'a O,
   brc20_store: &'a N,
+  brc30_store: &'a M,
 }
 
-impl<'a, O: OrdDataStoreReadWrite, N: BRC20DataStoreReadWrite> CallManager<'a, O, N> {
-  pub fn new(ord_store: &'a O, brc20_store: &'a N) -> Self {
+impl<'a, O: OrdDataStoreReadWrite, N: BRC20DataStoreReadWrite, M: BRC30DataStoreReadWrite>
+  CallManager<'a, O, N, M>
+{
+  pub fn new(ord_store: &'a O, brc20_store: &'a N, brc30_store: &'a M) -> Self {
     Self {
       ord_store,
       brc20_store,
+      brc30_store,
     }
   }
 
@@ -30,15 +44,25 @@ impl<'a, O: OrdDataStoreReadWrite, N: BRC20DataStoreReadWrite> CallManager<'a, O
     };
 
     match receipt {
-      Receipt::BRC20(_receipt) => {
-        // TODO: Internal message call from BRC20 to BRC30
-        // if enable_brc30 {
-        //   if let Ok(event) = receipt.result {
-        //     if let BRC20Event::Transfer(transfer) = event {
-        //       self.execute_message(BRC30::convert_message(receipt));
-        //     }
-        //   }
-        // }
+      Receipt::BRC20(brc20_receipt) => {
+        match brc20_receipt.result {
+          Ok(BRC20Event::InscripbeTransfer(brc20_transfer)) => {
+            //todo need convert amt with decimal
+            //let amt = convert_pledged_tick_with_decimal()
+            let passive_unstake = BRC30Operation::PassiveUnStake(PassiveUnStake {
+              stake: brc20_transfer.tick.as_str().to_string(),
+              amount: brc20_transfer.amount.to_string(),
+            });
+            // let mut msg = msg.clone();
+          }
+          Err(e) => {
+            // if err ,we log it
+            log::error!("error log:{}", e);
+          }
+          _ => {
+            //others operation ,we we do nothing
+          }
+        }
         Ok(())
       }
     }

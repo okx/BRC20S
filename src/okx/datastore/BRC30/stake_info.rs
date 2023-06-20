226f2517b91d2c5f703524f9f119cd877fd47e10
BananaLF <864685021@qq.com>
2023-06-20 23:27:01 +0800

    remove useless filed of stake_info (#35)

--- src/okx/datastore/BRC30/stake_info.rs
+++ src/okx/datastore/BRC30/stake_info.rs
@@ -4,23 +4,14 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 pub struct StakeInfo {
   pub stake: PledgedTick,
-  pub max_share: u128,
-  pub total_only: u128,
-  pub pool_stakes: Vec<(Pid,bool,u128)>,
+  pub pool_stakes: Vec<(Pid, bool, u128)>,
 }
 
 impl StakeInfo {
-  pub fn new(
-    pool_stakes: &Vec<(Pid,bool,u128)>,
-    stake: &PledgedTick,
-    max_share:u128,
-    total_only:u128,
-  ) -> Self {
+  pub fn new(pool_stakes: &Vec<(Pid, bool, u128)>, stake: &PledgedTick) -> Self {
     Self {
-      stake:stake.clone(),
-      pool_stakes:pool_stakes.clone(),
-      max_share:max_share,
-      total_only:total_only,
+      stake: stake.clone(),
+      pool_stakes: pool_stakes.clone(),
     }
   }
 }

22973c09263e582ef951be7db5e363078b325eb0
jianguo <zjg555543@163.com>
2023-06-21 12:19:48 +0800

    add

--- src/subcommand/server/brc30_types.rs
+++ src/subcommand/server/brc30_types.rs
@@ -64,7 +64,7 @@ pub struct BRC30Pool {
   pub minted: String,
   pub dmax: String,
   pub only: u64,
-  pub acc_per_share: u64,
+  pub acc_per_share: String,
   pub latest_update_block: u64,
   pub inscription_id: String,
   pub inscription_number: u64,
@@ -72,6 +72,42 @@ pub struct BRC30Pool {
   pub deploy_height: u64,
   pub deploy_blocktime: u64,
   pub txid: String,
+}
+
+impl From<&BRC30::PoolInfo> for BRC30Pool {
+  fn from(pool_info: &BRC30::PoolInfo) -> Self {
+    // TODO
+    let stake = Stake {
+      type_field: "".to_string(),
+      tick: "".to_string(),
+    };
+
+    //Todo
+    let earn = Earn {
+      id: "".to_string(),
+      name: "".to_string(),
+    };
+
+    Self {
+      pid: pool_info.pid.as_str().to_string(),
+      stake,
+      earn,
+      pool: pool_info.ptype.to_string(),
+      staked: pool_info.staked.to_string(),
+      erate: pool_info.erate.to_string(),
+      minted: pool_info.minted.to_string(),
+      dmax: pool_info.dmax.to_string(),
+      only: if (pool_info.only) { 0 } else { 1 },
+      acc_per_share: pool_info.acc_reward_per_share.to_string(),
+      latest_update_block: pool_info.last_update_block,
+      inscription_id: pool_info.inscription_id.to_string(),
+      inscription_number: 0,    // TODO inscription_number
+      deployer: "".to_string(), //TODO
+      deploy_height: 0,         //TODO
+      deploy_blocktime: 0,      // TODO  add deploy_blocktime
+      txid: pool_info.inscription_id.txid.to_string(),
+    }
+  }
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -104,6 +140,17 @@ pub struct UserInfo {
   pub latest_update_block: u64,
 }
 
+impl From<&BRC30::UserInfo> for UserInfo {
+  fn from(user_info: &BRC30::UserInfo) -> Self {
+    Self {
+      pid: user_info.pid.as_str().to_string(),
+      staked: user_info.staked.to_string(),
+      reward_debt: user_info.reward_debt.to_string(),
+      latest_update_block: user_info.latest_updated_block,
+    }
+  }
+}
+
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct BRC30Balance {
@@ -111,6 +158,22 @@ pub struct BRC30Balance {
   pub transferable: String,
   pub overall: String,
   pub claimable: String,
+}
+
+impl From<&BRC30::Balance> for BRC30Balance {
+  fn from(balance: &BRC30::Balance) -> Self {
+    let tick = Tick {
+      id: balance.tick_id.to_lowercase().hex(),
+      name: "".to_string(), //TODO
+    };
+
+    Self {
+      tick,
+      transferable: balance.transferable_balance.to_string(),
+      overall: balance.overall_balance.to_string(),
+      claimable: "0".to_string(), //TODO
+    }
+  }
 }
 
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
@@ -135,6 +198,23 @@ pub struct Inscription {
   pub owner: String,
 }
 
+impl From<&BRC30::TransferableAsset> for Inscription {
+  fn from(asset: &BRC30::TransferableAsset) -> Self {
+    let tick = Tick {
+      id: asset.tick_id.to_lowercase().hex(),
+      name: "".to_string(), //TODO
+    };
+
+    Self {
+      tick,
+      inscription_id: asset.inscription_id.to_string(),
+      inscription_number: 0,   //TODO
+      amount: "0".to_string(), //TODO
+      owner: asset.owner.to_string(),
+    }
+  }
+}
+
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Events {
@@ -147,15 +227,15 @@ pub struct Events {
 pub struct Event {
   #[serde(rename = "type")]
   pub type_field: String,
-  pub tick: Option<Tick>,
-  pub supply: Option<String>,
-  pub decimal: Option<u64>,
+  pub tick: Tick,
+  pub supply: String,
+  pub decimal: u64,
   pub inscription_number: u64,
   pub inscription_id: String,
   pub old_satpoint: String,
   pub new_satpoint: String,
-  pub from: AddFrom,
-  pub to: To,
+  pub from: String,
+  pub to: String,
   pub valid: bool,
   pub msg: String,
   pub pid: String,
@@ -168,21 +248,51 @@ pub struct Event {
   pub amount: String,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct AddFrom {
-  pub address: String,
+impl From<&BRC30::BRC30Receipt> for Event {
+  fn from(receipt: &BRC30::BRC30Receipt) -> Self {
+    // TODO
+    let earn = Earn {
+      id: "".to_string(),
+      name: "".to_string(),
+    };
+
+    let stake = Stake {
+      type_field: "".to_string(),
+      tick: "".to_string(),
+    };
+
+    let tick = Tick {
+      id: "".to_string(),
+      name: "".to_string(),
+    };
+
+    Self {
+      type_field: "".to_string(),
+      tick,
+      supply: "".to_string(),
+      decimal: 0,
+      inscription_number: 0,
+      inscription_id: "".to_string(),
+      old_satpoint: "".to_string(),
+      new_satpoint: "".to_string(),
+      from: "".to_string(),
+      to: "".to_string(),
+      valid: false,
+      msg: "".to_string(),
+      pid: "".to_string(),
+      stake,
+      earn,
+      pool: "".to_string(),
+      erate: "".to_string(),
+      only: 0,
+      dmax: "".to_string(),
+      amount: "".to_string(),
+    }
+  }
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct To {
-  pub address: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct BlockEvents {
-  pub txid: String,
-  pub events: Vec<Event>,
+pub struct BRC30BlockEvents {
+  pub block: Vec<Event>,
 }

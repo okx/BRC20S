ceab627cb8fd5559af93772283cebb85724bd70b
FineKe <530362991@qq.com>
2023-07-31 17:20:48 +0800

    revert sh

--- dev/private-start.sh
+++ dev/private-start.sh
@@ -1,1 +1,2 @@
-../target/release/ord --log-level=DEBUG --data-dir=./_cache --index-sats --rpc-url=http://18.167.77.79:18443 --regtest --bitcoin-rpc-user bitcoinrpc --bitcoin-rpc-pass bitcoinrpc  server
+rm -rf ./_cache
+../target/release/ord --log-level=INFO --data-dir=./_cache --index-sats --rpc-url=<url>--regtest --bitcoin-rpc-user <username> --bitcoin-rpc-pass <password> server

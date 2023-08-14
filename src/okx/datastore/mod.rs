f2697559b33cc19cb674a11d2ac4db881933def4
ylsGit <bzwdyls@163.com>
2023-08-14 11:05:12 +0800

    Update mod.rs

--- src/okx/datastore/mod.rs
+++ src/okx/datastore/mod.rs
@@ -4,11 +4,10 @@ pub mod brc20s;
 pub mod btc;
 pub mod ord;
 mod script_key;
-pub mod btc;
 
 pub use self::{
   brc20::DataStoreReadWrite as BRC20DataStoreReadWrite,
-  brc20s::DataStoreReadWrite as BRC20SDataStoreReadWrite, ord::OrdDataStoreReadWrite,
-  btc::DataStoreReadWrite as BTCDataStoreReadWrite,
+  brc20s::DataStoreReadWrite as BRC20SDataStoreReadWrite,
+  btc::DataStoreReadWrite as BTCDataStoreReadWrite, ord::OrdDataStoreReadWrite,
   script_key::ScriptKey,
 };

d888d4d9cca3175896f0d21aff8402b25fc9ff1b
BananaLF <864685021@qq.com>
2023-09-26 11:12:10 +0800

    update scripts

--- dev/private-start.sh
+++ dev/private-start.sh
@@ -1,2 +1,2 @@
 rm -rf ./_cache
-../target/release/ord --log-level=INFO --data-dir=./_cache --index-sats --rpc-url=<url>--regtest --bitcoin-rpc-user <username> --bitcoin-rpc-pass <password> server
+../target/release/ord --log-level=INFO --data-dir=./_cache --index-sats --rpc-url=<url> --regtest --bitcoin-rpc-user <user> --bitcoin-rpc-pass <password> --brczero-rpc-url <url> --first-brczero-height 1500 server
